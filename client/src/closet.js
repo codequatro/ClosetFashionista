e7f9467baba1ecbad18fc3f585e02bdf6f59d1fe
Mike Mitchel <mike@mikemitchel.com>
2015-12-18 13:53:54 -0600

    more front end fixes

--- client/src/closet.js
+++ client/src/closet.js
@@ -1,6 +1,8 @@
 'use strict';
+var url = 'http://www.fantasticviewpoint.com/wp-content/uploads/2013/09/street-style-style-motivation-13-620x925.jpg'
 
 angular.module('myApp')
   .controller('ClosetCtrl', ['$scope','$http', function($scope,$http) {
     $scope.header = 'You will find your closet here';
+    $scope.imageUrl = url;
   }]);
