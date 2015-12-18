cbae3f316f118f091a786ba9a48adc2d8398e00e
Mike Mitchel <mike@mikemitchel.com>
2015-12-18 17:17:56 -0600

    basic flow from home, to signin to closet

--- client/src/auth.js
+++ client/src/auth.js
@@ -3,4 +3,17 @@
 angular.module('myApp')
   .controller('AuthCtrl', ['$scope','$http', function($scope,$http) {
     $scope.header = 'this will be the auth page';
+    $scope.master = {}
+
+    $scope.signin = function(user) {
+      console.log(user)
+
+    }
+
+    $scope.reset = function() {
+      $scope.user = angular.copy($scope.master)
+    }
+
+    $scope.reset();
+
   }]);
