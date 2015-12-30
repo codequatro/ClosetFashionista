a9f8897ea12f6a1d3deeeeae295ddfefa972542f
eazy-g <gbrustkern@gmail.com>
2015-12-30 00:40:39 -0600

    closes codequatro/codequatro#53

--- client/src/closet.js
+++ client/src/closet.js
@@ -7,11 +7,36 @@ angular.module('myApp')
     $scope.username = $window.localStorage.getItem('username');
 
     $scope.getCloset = function(){
+
+      //Call the factory method which gets a users images and votes for those images
       Register.register.getCloset($scope.username)
       .then(function(data){
-        //data.images is the array of objects. Need to pull image_name from each one of these objects
-        $scope.pics = data.images;
-      });
+
+        /*************this needs to be moved into the factory******************/
+        $scope.pics = data.pics;  //data.pics is an array of the users images
+        //for each picture, we have an inner for loop that checks every vote to see if it belongs to the current picture
+        for(var j = 0; j<data.pics.length; j++){
+          $scope.pics[j].total = 0; //total number of votes
+          $scope.pics[j].yes = 0; //total number of 'up' votes
+            //loop through every vote that belongs to one of the user's pictures
+            for(var i = 0; i<data.votes.length; i++){
+              var row = data.votes[i]; //data.votes is an array of objects, so this grabs the individual object
+              var vote = row["vote"]; //value is either a 1 for 'up' or 0 for 'down' vote
+              var imageName = row["image_name"];
+              if($scope.pics[j].image_name === imageName){
+                if(vote === 1){
+                  $scope.pics[j].yes += 1;
+                  $scope.pics[j].total += 1;
+                }
+                else{
+                  $scope.pics[j].total += 1;
+                }
+            }
+          }//end first for loop
+        }
+        console.log('scope pics later on', $scope.pics);
+        /*************this needs to be moved into the factory******************/
+      }); //end .then
     };
 
   }]);
