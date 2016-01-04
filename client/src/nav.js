8ac32c22e955adbe997229c7a76981c24ac02421
Mike Mitchel <mike@mikemitchel.com>
2016-01-04 10:17:18 -0600

    merges

--- client/src/nav.js
+++ client/src/nav.js
@@ -2,6 +2,5 @@
 
 angular.module('myApp')
   .controller('NavCtrl', ['$scope','$window', 'Authorization', function($scope, $window, Authorization) {
-   $scope.nugget='NavController hijack!!'
    $scope.isAuth=Authorization.authorized;
   }]);
