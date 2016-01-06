47bc22d69682ef21731b2591ccd897194d534b99
GreenRaccoon23 <GreenRaccoon23@gmail.com>
2016-01-06 11:59:41 -0600

    Started to promisify functions in images controller to help readability

--- server/images/imagesController.js
+++ server/images/imagesController.js
@@ -15,24 +15,39 @@ var cheerio = require('cheerio');
 
 exports = module.exports = {
 
+	pgConnect: function() {
+		var deferred = Q.defer();
+    pg.connect(connectString, function (err, client, done){
+      if(err){
+        deffered.reject(new Error('error connecting to the DB:', err) );
+      } else {
+      	deferred.resolve({
+      		client: client,
+      		done: done
+      	})
+      }
+    })
+    return deferred.promise;
+	},
+
 	postImage: function(req, res, next) {
 		var form = new formidable.IncomingForm();
 
 		form.parse(req, function(err, fields, files) {
+			//this is not the right way to go about it. url gets wierd
+			//NEED TO FIX
 
-		//this is not the right way to go about it. url gets wierd
-		//NEED TO FIX
+			res.redirect('back');
 
-		res.redirect('back');
-
-		//if you want to look at the form getting sent to the server
-		// res.end(util.inspect({fields: fields, files: files}));
+			//if you want to look at the form getting sent to the server
+			// res.end(util.inspect({fields: fields, files: files}));
 		});
 
 		var myFields = {};
+
 		form.on('field', function(field, value){
-		myFields[field] = value;
-		console.log('fields in form.on', myFields);
+			myFields[field] = value;
+			console.log('fields in form.on', myFields);
 		});
 
 		form.on('end', function(fields, files) {
@@ -45,36 +60,74 @@ exports = module.exports = {
 			/* Location where we want to copy the uploaded file */
 			var new_location = './client/uploads/';
 
-			fs.copy(temp_path, new_location + file_name, function(err) {
-			  if (err) {
-			    console.error(err);
-			  }
-			  else {
-			    pg.connect(connectString, function (err, client, done){
-			      if(err){
-			        console.error('error connecting to the DB:', err);
-			      }
-			      // console.log('username', username);
-			      client.query('SELECT user_id FROM users WHERE username = $1', [username], function(err, result){
-			        var user_id = result.rows[0].user_id;
-			        if(err){
-			          console.error('error on lookup of user id:', err)
-			        }
-			        else
-			        {
-			          // console.log('select user result', result);
-			          client.query('INSERT INTO images (image_name, user_id, type_id) VALUES ($1, $2, $3)', [file_name, user_id, clothing_type], function (err, result){
-			            if(err){
-			              console.error(err);
-			            }else {
-			              done();
-			            }
-			          })
-			        }
-			      });
-			    })
-			  }
-			}); //fs copy end
+			var fsCopy = Q.nbind(fs.copy, fs);
+
+			var clientQuery;
+			var done;
+
+			fsCopy( temp_path, new_location + file_name )
+				.then(exports.pgConnect)
+	    	.then(function(connection) {
+	    		done = connection.done;
+	    		clientQuery = Q.nbind(connection.client.query, connection.client);
+		      // if(err){
+		      //   throw new Error('error connecting to the DB:', err);
+		      // }
+
+		      // console.log('username', username);
+		    	// var clientQuery = Q.nbind(client.query, client);
+
+		      return clientQuery(`
+		      	SELECT user_id FROM users
+		      		 WHERE username = $1`,
+		      	[username]
+		      );
+		    })
+      	.then(function(result) {
+        	var user_id = result.rows[0].user_id;
+          // console.log('select user result', result);
+          return clientQuery(`
+          	INSERT INTO images (image_name, user_id, type_id)
+          		VALUES ($1, $2, $3)`,
+          	 [file_name, user_id, clothing_type]
+          );
+      	})
+	      .then(done)
+		    .fail(function(err) {
+	        console.log('fsCopy err');
+        	console.error(err);
+        }); //fs copy end
+
+			// fs.copy(temp_path, new_location + file_name, function(err) {
+			//   if (err) {
+			//     console.error(err);
+			//   }
+			//   else {
+			//     pg.connect(connectString, function (err, client, done){
+			//       if(err){
+			//         console.error('error connecting to the DB:', err);
+			//       }
+			//       // console.log('username', username);
+			//       client.query('SELECT user_id FROM users WHERE username = $1', [username], function(err, result){
+			//         var user_id = result.rows[0].user_id;
+			//         if(err){
+			//           console.error('error on lookup of user id:', err)
+			//         }
+			//         else
+			//         {
+			//           // console.log('select user result', result);
+			//           client.query('INSERT INTO images (image_name, user_id, type_id) VALUES ($1, $2, $3)', [file_name, user_id, clothing_type], function (err, result){
+			//             if(err){
+			//               console.error(err);
+			//             }else {
+			//               done();
+			//             }
+			//           })
+			//         }
+			//       });
+			//     })
+			//   }
+			// }); //fs copy end
 		}) //form.on 'end' end
 		
 	},
@@ -144,12 +197,20 @@ exports = module.exports = {
 
 	vote: function(req, res, next) {
 		var username = req.body.username;
-<<<<<<< HEAD
-		var hotOrNot = req.body.hotOrNot;
-=======
 		var voteValue = req.body.voteValue;
->>>>>>> master
 		var imageId = req.body.imageId;
+		var upvote = 0;
+		var downvote = 0;
+		var flag = 0;
+		if (voteValue === 'upvote') {
+			upvote++
+		} else if (voteValue === 'downvote') {
+			downvote++;
+		} else if (voteValue === 'flag') {
+			flag++;
+		} else {
+			console.log('something terrible happened and the zombies are coming!')
+		}
 		console.log('imageId', imageId);
 		pg.connect(connectString, function (err, client, done) {
 		if(err){
@@ -162,11 +223,7 @@ exports = module.exports = {
 		    }
 		    else {
 		      var userId = result.rows[0].user_id
-<<<<<<< HEAD
-		      client.query('INSERT INTO votes (user_id, image_id, vote) VALUES ($1, $2, $3)',[userId, imageId, hotOrNot], function(err, result){
-=======
 		      client.query('INSERT INTO votes (user_id, image_id, upvote, downvote, flag) VALUES ($1, $2, $3, $4, $5)',[userId, imageId, upvote, downvote, flag], function(err, result){
->>>>>>> master
 		        if(err){
 		          console.error('error inserting vote into votes table: ', err);
 		        }
@@ -181,9 +238,6 @@ exports = module.exports = {
 		});
 	},
 
-<<<<<<< HEAD
-	
-=======
 	getAllImages: function (req, res, next) {
 		//create an object to send back to client
 		var allImages = {};
@@ -220,6 +274,69 @@ exports = module.exports = {
 	getImageData: function (req, res, next) {
 		
 	}
->>>>>>> master
 
 }
+
+// postImage: function(req, res, next) {
+// 		var form = new formidable.IncomingForm();
+
+// 		form.parse(req, function(err, fields, files) {
+
+// 		//this is not the right way to go about it. url gets wierd
+// 		//NEED TO FIX
+
+// 		res.redirect('back');
+
+// 		//if you want to look at the form getting sent to the server
+// 		// res.end(util.inspect({fields: fields, files: files}));
+// 		});
+
+// 		var myFields = {};
+// 		form.on('field', function(field, value){
+// 		myFields[field] = value;
+// 		console.log('fields in form.on', myFields);
+// 		});
+
+// 		form.on('end', function(fields, files) {
+// 			var username = myFields.name;
+// 			var clothing_type = myFields.clothingType;
+// 			/* Temporary location of our uploaded file */
+// 			var temp_path = this.openedFiles[0].path;
+// 			/* The file name of the uploaded file */
+// 			var file_name = this.openedFiles[0].name;
+// 			/* Location where we want to copy the uploaded file */
+// 			var new_location = './client/uploads/';
+
+// 			fs.copy(temp_path, new_location + file_name, function(err) {
+// 			  if (err) {
+// 			    console.error(err);
+// 			  }
+// 			  else {
+// 			    pg.connect(connectString, function (err, client, done){
+// 			      if(err){
+// 			        console.error('error connecting to the DB:', err);
+// 			      }
+// 			      // console.log('username', username);
+// 			      client.query('SELECT user_id FROM users WHERE username = $1', [username], function(err, result){
+// 			        var user_id = result.rows[0].user_id;
+// 			        if(err){
+// 			          console.error('error on lookup of user id:', err)
+// 			        }
+// 			        else
+// 			        {
+// 			          // console.log('select user result', result);
+// 			          client.query('INSERT INTO images (image_name, user_id, type_id) VALUES ($1, $2, $3)', [file_name, user_id, clothing_type], function (err, result){
+// 			            if(err){
+// 			              console.error(err);
+// 			            }else {
+// 			              done();
+// 			            }
+// 			          })
+// 			        }
+// 			      });
+// 			    })
+// 			  }
+// 			}); //fs copy end
+// 		}) //form.on 'end' end
+		
+// 	},
